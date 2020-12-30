91d2a07885cc2b9199824457c878ac2131358126
crackwebai1024 <webtowardapex@outlook.com>
2020-12-30 09:06:02 +0800

    arrange some and add signin and get data by page and count

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 8081,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
     process.env.MONGODB_URI ||
